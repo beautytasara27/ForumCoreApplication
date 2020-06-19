3fe92e415175f6344b8966596951b38807ab942f
beautytasara27 <43332045+beautytasara27@users.noreply.github.com>
2020-06-19 17:19:24 +0700

    Update ArticleRestController.java

--- src/main/java/com/binus/project/forumcoreapplication/web/ArticleRestController.java
+++ src/main/java/com/binus/project/forumcoreapplication/web/ArticleRestController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("v1/articles")
-@CrossOrigin(origins = {"https://5ee67ef494e11be92b99cf62--blissful-mayer-5672f4.netlify.app","http://localhost:3000"})
+@CrossOrigin(origins = {"https://mentalhealthfirst.social","http://localhost:3000"})
 class ArticleRestController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ArticleService.class);
