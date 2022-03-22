bb5950d48924601ba3914f890949e0616d41e9e2
beautytasara27 <43332045+beautytasara27@users.noreply.github.com>
2022-03-22 14:18:38 +0700

    Cors in Articles

--- src/main/java/com/binus/project/forumcoreapplication/web/ArticleRestController.java
+++ src/main/java/com/binus/project/forumcoreapplication/web/ArticleRestController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("v1/articles")
-@CrossOrigin(origins = {"https://mentalhealthfirst.social","http://localhost:3000"})
+@CrossOrigin(origins = {"https://mentalhealthfirst.netlify.app","http://localhost:3000"})
 class ArticleRestController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ArticleService.class);
